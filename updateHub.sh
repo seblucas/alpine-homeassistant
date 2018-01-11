0f5715373d6e6a57c4cb59b721e619ae4b414d59
Sébastien Lucas <sebastien@slucas.fr>
2018-01-11 10:19:47 +0100

    Update the helper to handle the manifest

--- updateHub.sh
+++ updateHub.sh
@@ -2,18 +2,23 @@
 
 TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 MAJMIN=`echo $TAG | cut -d . -f 1,2 `
+DIR="alpine-homeassistant"
 
 echo $TAG
 echo $MAJMIN
 
-NBARCH=`manifest-tool inspect seblucas/alpine-homeassistant:$TAG | grep "Arch:" | wc -l`
+NBARCH=`manifest-tool inspect seblucas/$dir:$TAG | grep "Arch:" | wc -l`
 if [[ $NBARCH -gt 1 ]]; then
   echo "Image seblucas/$dir:$TAG already uptodate"
-  continue
+  exit 0
 fi
-docker build . -t seblucas/alpine-homeassistant:armv6-$TAG
-docker push seblucas/alpine-homeassistant:armv6-$TAG
-sed -i "s|{tag}|$TAG|g" manifest.yaml
-sed -i "s|{majmin}|$MAJMIN|g" manifest.yaml
-manifest-tool push from-spec manifest.yaml
-git checkout manifest.yaml
+docker build . -t seblucas/$dir:$ARCH-$TAG -t seblucas/$dir:$MAJMIN
+docker push seblucas/$dir:$ARCH-$TAG
+if [[ $ARCH == "armhf" ]]; then
+  echo "Manifest uploading ..."
+  sed -i "s|{image}|$dir|g" manifest.yaml
+  sed -i "s|{tag}|$TAG|g" manifest.yaml
+  sed -i "s|{majmin}|$MAJMIN|g" manifest.yaml
+  manifest-tool push from-spec manifest.yaml
+  git checkout manifest.yaml
+fi
